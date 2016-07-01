978a68d171517af542e5e2b045b7af78adabe1aa
Fedor Indutny <fedor@indutny.com>
2016-07-01 03:00:56 -0400

    cli: fix windows again

--- test/cli-test.js
+++ test/cli-test.js
@@ -6,13 +6,13 @@ const path = require('path');
 const ninja = require('../');
 const cli = ninja.cli;
 
-const file = path.join(__dirname, 'fixtures', 'win.env');
+const fixtures = path.join(__dirname, 'fixtures');
 
 describe('ninja/CLI', () => {
   describe('.prepareWinEnv()', () => {
     it('should not change non-prefixed cmd', () => {
       const options = {};
-      const cmd = cli.prepareWinEnv('abc', options, {}, new Map());
+      const cmd = cli.prepareWinEnv('abc', options, '.', {}, new Map());
       assert.equal(cmd, 'abc');
       assert.deepEqual(options, {});
     });
@@ -20,18 +20,18 @@ describe('ninja/CLI', () => {
     it('should change prefixed cmd', () => {
       const options = {};
       const cache = new Map();
-      const cmd = cli.prepareWinEnv(`ninja -t msvc -e ${file} -- abc`, options,
-                                    {}, cache);
+      const cmd = cli.prepareWinEnv(`ninja -t msvc -e win.env -- abc`, options,
+                                    fixtures, {}, cache);
       assert.equal(cmd, ' abc');
       assert.deepEqual(options, { env: { ABC: 123, DEF: 456 } });
-      assert.deepEqual(cache.get(file), {  ABC: 123, DEF: 456 });
+      assert.deepEqual(cache.get('win.env'), {  ABC: 123, DEF: 456 });
     });
 
     it('should replace when called without -e', () => {
       const options = {};
       const cache = new Map();
       const cmd = cli.prepareWinEnv(`ninja -t msvc -- abc`, options,
-                                    {}, cache);
+                                    '.', {}, cache);
       assert.equal(cmd, ' abc');
       assert.deepEqual(options, {});
     });
